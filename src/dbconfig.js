2f5518a3c110ab4c47451ac924ac8736598af0de
raihan2bd <raihan2bd.official@gmail.com>
2023-10-01 19:17:40 +0600

    Integrate firebase to the project

---
+++ src/dbconfig.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+const apiKey = process.env.API_KEY
+const authDomain = process.env.AUTH_DOMAIN
+const databaseURL = process.env.DB_URL
+const projectId = process.env.PROJECT_ID
+const storageBucket = process.env.STORAGE_BUCKET
+const messagingSenderId = process.env.MESSAGING_SENDER_ID
+const appId = process.env.APP_ID
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
