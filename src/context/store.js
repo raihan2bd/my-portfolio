8098477347cbd4ba6be4b5965ef7a0d477420916
raihan2bd <raihan2bd.official@gmail.com>
2023-10-14 20:31:51 +0600

    Fix store from the context provider

--- src/context/store.js
+++ src/context/store.js
@@ -1,15 +1,45 @@
-"use client";
-
+'use client'
+import React, { useEffect } from "react";
 import { getCookie } from "cookies-next";
-import { useState, useContext, createContext } from "react";
+import { createContext, useState, useContext } from "react";
 
-const AuthContext = createContext({
-  uid: null,
-  role: null
-});
+export const AuthContext = createContext({});
 
 export const ContextProvider = ({ children }) => {
-  const [user, setUser] = useState(AuthContext);
+  const [user, setUser] = useState({
+    uid: getCookie("uid") || null,
+    role: getCookie("role") || null,
+  });
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    // Monitor changes in cookies and update context if necessary
+    const handleCookieChange = () => {
+      const newUid = getCookie("uid") || null;
+      const newRole = getCookie("role") || null;
+
+      if (newUid !== user.uid || newRole !== user.role) {
+        setUser({ uid: newUid, role: newRole });
+      }
+    };
+
+    // Add event listener to detect changes in cookies
+    window.addEventListener("storage", handleCookieChange);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("storage", handleCookieChange);
+    };
+  }, [user.uid, user.role]);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
+
+export const useGlobalState = () => {
+  return useContext(AuthContext);
+};
+
+
